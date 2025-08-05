433b99ab84f94ec0aab66d995b11eb425731b823
Ismaillhan2007 <159274473+Ismaillhan2007@users.noreply.github.com>
2025-08-06 00:48:56 +0500

    Создал кнопку для регистрации для будущего,наладил css между кнопками,добавил отступы

--- my-app/src/components/Main.tsx
+++ my-app/src/components/Main.tsx
@@ -85,14 +85,20 @@ const Main =()=>{
     };
 
     return(
-        <div className="p-8">
+        <div className="p-8 ">
             <h1 className="text-3xl font-bold mb-6">Добро пожаловать на главную страницу!</h1>
+            <div className="gap-4">
             <button 
                 onClick={()=>setIsModalOpen(true)}
-                className="static bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                className="static bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-3"
             >
                 Start chat 
             </button>
+            <button className="static bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-3">
+                Registration
+            </button>
+            </div>
+           
             <Modal isOpen={isModalOpen} OnClose={() => setIsModalOpen(false)}>
                 <div className="w-96 h-96 flex flex-col">
                     <h2 className="text-xl font-bold mb-4">AI Chat</h2>
